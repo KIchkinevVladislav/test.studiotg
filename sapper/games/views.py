d65b8b55a8bf068348cec71aebbed76bec271bdc
Владислав <kant1755@mail.ru>
2024-02-14 00:11:22 +0300

    readme

--- sapper/games/views.py
+++ sapper/games/views.py
@@ -1,7 +1,8 @@
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
+
 from .serializers import GameSerializer
 from .models import Game
 
